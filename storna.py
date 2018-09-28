874b3d2d9d2e8d054fc908fc7145ac52cf962076
Konstanty Kordas <Andrzej@AndrzejKordas.wenedow.local>
2018-09-28 16:30:21 +0200

    GENERAL KENOBI

--- storna.py
+++ storna.py
@@ -7,5 +7,10 @@ def hello():
     return 'Hello, World!'
 
 
-if __name__ == '__main__':
+@app.route('/app/')
+def hi():
+    return "Hello there"
+
+
+if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1")
