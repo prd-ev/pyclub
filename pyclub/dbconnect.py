83360716bf375e72dca5e07bad66537d74e52801
tykazzz <tlakomy2001@gmail.com>
2018-10-03 19:44:54 +0200

    Add function create_db

--- pyclub/dbconnect.py
+++ pyclub/dbconnect.py
@@ -3,10 +3,10 @@ import pymysql
 __author__ = "Tomasz Lakomy"
 
 def connection():
-	conn = pymysql.connect(host='localhost', 
-						   user='tykaz', 
-						   password='', 
-						   db='pyclub', 
+	conn = pymysql.connect(host='localhost',
+						   user='tykaz',
+						   password='',
+						   db='pyclub',
 						   charset='utf8mb4',
 						   cursorclass=pymysql.cursors.DictCursor)
 	c = conn.cursor()
@@ -16,8 +16,15 @@ def connection():
 def get_user(userid):
 	"""Function takes userid and returns dict with data from database"""
 	c, conn = connection()
-	c.execute("select * from user where iduser=%s", pymysql.escape_string(str(userid)))
-	z = c.fetchone()
+	c.execute("SELECT * FROM user WHERE iduser=%s", pymysql.escape_string(str(userid)))
+	user_data = c.fetchone()
 	c.close()
 	conn.close()
 	return user_data
+
+def create_db():
+	"""Function creates database from file create-database_pyclub.py"""
+	c, conn = connection()
+	c.execute("SOURCE sql/create-database_pyclub.sql")
+	c.close()
+	conn.close()
