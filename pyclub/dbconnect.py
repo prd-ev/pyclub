c3dc2b6a591eff605f415328a77e16252ebfc256
tykazzz <tlakomy2001@gmail.com>
2018-10-03 21:19:36 +0200

    fixed dbconnect syntax

--- pyclub/dbconnect.py
+++ pyclub/dbconnect.py
@@ -4,11 +4,11 @@ __author__ = "Tomasz Lakomy"
 
 def connection():
 	conn = pymysql.connect(host='localhost',
-						   user='tykaz',
-						   password='',
-						   db='pyclub',
-						   charset='utf8mb4',
-						   cursorclass=pymysql.cursors.DictCursor)
+			       user='tykaz',
+			       password='',
+			       db='pyclub',
+			       charset='utf8mb4',
+			       cursorclass=pymysql.cursors.DictCursor)
 	c = conn.cursor()
 	return c, conn
 
