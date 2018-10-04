e662b04495c93b0b07aa7f0cbc498fdc9d0f4841
tykazzz <tlakomy2001@gmail.com>
2018-10-04 23:42:45 +0200

    Add get_* and create_* functions

--- pyclub/dbconnect.py
+++ pyclub/dbconnect.py
@@ -1,5 +1,6 @@
 import pymysql
 from pymysql import escape_string
+from flask_login._compat import unicode
 
 __author__ = "Tomasz Lakomy"
 
@@ -12,16 +13,6 @@ def connection():
 			       cursorclass=pymysql.cursors.DictCursor)
 	c = conn.cursor()
 	return c, conn
-
-
-def get_user(userid):
-	"""Function takes userid and returns dict with data from database"""
-	c, conn = connection()
-	c.execute("SELECT * FROM user WHERE iduser=%s", pymysql.escape_string(str(userid)))
-	user_data = c.fetchone()
-	c.close()
-	conn.close()
-	return user_data
 
 def create_db():
 	"""Function creates database from file create-database_pyclub.py"""
@@ -52,4 +43,68 @@ def create_organization(name, contact):
 	c.close()
 	conn.close()
 
-create_organization('fajnaorganizacja',' marczin@zsk.pl')
+def create_club(info, organization_id):
+	"""Function creates club"""
+	c, conn = connection()
+	c.execute("INSERT INTO club (info, organization_id) VALUES"
+			  "(%s, %s)"
+			  , (escape_string(str(info)), escape_string(str(organization_id)))
+	)
+	conn.commit()
+	c.close()
+	conn.close()
+
+def create_event(date, info, club_id):
+	"""Function creates event"""
+	c, conn = connection()
+	c.execute("INSERT INTO event (date, info, club_id) VALUES"
+			  "(%s, %s)"
+			  , (escape_string(str(date)), escape_string(str(info)), (escape_string(str(club_id))))
+	)
+	conn.commit()
+	c.close()
+	conn.close()
+
+def get_user(userkey):
+	"""Function takes userid and returns dict with data from database"""
+	c, conn = connection()
+	c.execute("SELECT * FROM user WHERE iduser=%s or email=%s", ((escape_string(str(userkey))), (escape_string(str(userkey)))))
+	user_data = User()
+	user_data.update(c.fetchone())
+	c.close()
+	conn.close()
+	return user_data
+
+class User(dict):
+	def is_authenticated(self):
+		return True
+	def is_active(self):
+		return True
+	def is_anonymus(self):
+		return True
+	def get_id(self):
+		return unicode(self['iduser'])
+
+def get_organization(organizationkey):
+	c, conn = connection()
+	c.execute("SELECT * FROM organization WHERE idorganization=%s or name=%s", ((escape_string(str(organizationkey))), (escape_string(str(organizationkey)))))
+	organization_data = (c.fetchone())
+	c.close()
+	conn.close()
+	return organization_data
+
+def get_club(clubid):
+	c, conn = connection()
+	c.execute("SELECT * FROM club WHERE idclub=%s", escape_string(str(clubid)))
+	club_data = (c.fetchone())
+	c.close()
+	conn.close()
+	return club_data
+
+def get_event(eventid):
+	c, conn = connection()
+	c.execute("SELECT * FROM event WHERE idevent=%s", (escape_string(str(eventid))))
+	event_data = (c.fetchone())
+	c.close()
+	conn.close()
+	return event_data
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,20 @@
-from pyclub.dbconnect import get_user
+import pytest
+from pyclub.dbconnect import create_user, create_organization, create_club, create_event,  get_user, get_club, get_organization, get_event
+
+def test_create_user():
+	create_user('Firstname','Test','email@email.com','haslomaslo')
 
 def test_get_user():
 	user_data = get_user(1)
-	sql = {'iduser': 1, 'first_name': 'kk', 'last_name': 'bb', 'email': 'email@email.com', 'password': 'haslomaslo'}
+	sql = {'iduser': 1, 'first_name': 'Firstname', 'last_name': 'Test', 'email': 'email@email.com', 'password': 'haslomaslo'}
 	if user_data != sql:
+		raise AssertionError()
+
+def test_create_organization():
+	create_organization('nazwa','kontakt@org')
+
+def test_get_organization():
+	organization_data = get_organization(1)
+	sql = {'idorganization': 1, 'name': 'nazwa', 'contact': 'kontakt@org'}
+	if organization_data != sql:
 		raise AssertionError()
