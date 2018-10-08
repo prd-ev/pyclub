cd89f0e7863f208af4d3768ef6eddbea78c4183d
tykazzz <tlakomy2001@gmail.com>
2018-10-08 20:37:57 +0200

    fix #6

--- sql/update-to-1.1.sql
+++ sql/update-to-1.1.sql
@@ -1,4 +1,4 @@
--- Update db to v1.1
+-- Update db to v1.1 from v1.0
 -- Sun, 7 Oct 2018, 16:55:55
 -- Model: pyclub    Version: 1.1
 
@@ -8,3 +8,6 @@ ALTER TABLE user
 
 ALTER TABLE event_membership
     CHANGE COLUMN event_club_idclub own_club_id int(11);
+
+ALTER TABLE user 
+    ADD COLUMN email_confirm tinyint(1) NOT NULL;
