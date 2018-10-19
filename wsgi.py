b5dd0ec6d711a30ad43952d4c0fd1a73adb56917
Konstanty Kordas <Konstanty@AndrzejKordas.wenedow.local>
2018-10-19 17:18:12 +0200

    login without hash, basic session (wip)

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask.sessions import SessionInterface
 from pyclub.dbconnect import *
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'cokolwiek'
 
 @app.route("/")
 def index_page():
-    return render_template("index.html")
+    qwe = None
+    if 'ID' in session:
+        qwe = '123'
+    return render_template("index.html", session_true = qwe)
 
 
 @app.route("/register/", methods = ["POST", "GET"])
@@ -19,7 +24,6 @@ def register_page():
         new_first_name = request.form['name']
         new_last_name = request.form['last_name']
         if new_email and new_password and new_first_name and new_last_name and new_password == new_password_confirm:
-            new_password = generate_password_hash(new_password)
             create_user(new_first_name, new_last_name, new_email, new_password)
         elif new_password != new_password_confirm:
             error_message = "Hasła muszą się zgadzać"
@@ -28,9 +32,23 @@ def register_page():
     return render_template("register.html", error = error_message)
 
 
-@app.route("/login/")
+@app.route("/login/", methods = ["POST", "GET"])
 def login_page():
+    if request.method == "POST":
+        attempted_email = request.form['email']
+        attempted_password = request.form['password']
+        test = get_user(str(attempted_email))
+        db_password = test.get('password')
+        db_id = test.get('iduser')
+        if db_password == attempted_password:
+            session['ID'] = db_id
+            return redirect(url_for('index_page'))
     return render_template("login.html")
+
+@app.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('index_page'))
 
 
 @app.route("/contact/")
@@ -43,11 +61,9 @@ def about_page():
     return render_template("about.html")
 
 
-@app.route("/test/")
+"""@app.route('/test/')
 def test():
-    user = User({"first_name":"asd"})
-    ID_new = user.get_id
-    return ID_new
+"""
 
 
 
