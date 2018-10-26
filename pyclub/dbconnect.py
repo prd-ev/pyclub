c6d23ad8c074ee242d00e98df92b2c53c1f8e89c
tykazzz <tlakomy2001@gmail.com>
2018-10-26 21:17:56 +0200

    Fix confirmation_mail funcion

--- pyclub/dbconnect.py
+++ pyclub/dbconnect.py
@@ -204,17 +204,6 @@ def get_user_to_club_membership(userid):
 	conn.close()
 	return userid
 
-def confirm_email(usermail):
-	'''Function confirms user's mail'''
-	c, conn = connection()
-	c.execute('UPDATE user SET email_confirm=1 WHERE email=%s', (escape_string(usermail)))
-	conn.commit()
-	c.close()
-	conn.close()
-
-if __name__ == '__main__':
-	create_user("Adam", "Korba", "korba.adam@gmail.com", "1234")
-
 def get_event_next_week():
 	"""Functions shows events from the next week
 
@@ -261,3 +250,11 @@ def get_event_next_month():
 	c.close()
 	conn.close()
 	return event_data
+
+def confirm_email(usermail):
+	'''Function confirms user's mail'''
+	c, conn = connection()
+	c.execute('UPDATE user SET email_confirm=1 WHERE email=%s', (escape_string(usermail)))
+	conn.commit()
+	c.close()
+	conn.close()
