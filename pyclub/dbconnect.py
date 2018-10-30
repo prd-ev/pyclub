5e0db6bd011efb373ea85874aa406fab9be340ff
tykazzz <tlakomy2001@gmail.com>
2018-10-30 12:52:22 +0100

    Add delete_membership functions

--- pyclub/dbconnect.py
+++ pyclub/dbconnect.py
@@ -88,18 +88,18 @@ def del_user(userid):
 		tip: check dependencies before deleting
 	"""
 	c, conn = connection()
-	c.execute('DELETE from user WHERE iduser=%s;', (escape_string(str(userid))))
+	c.execute('DELETE FROM user WHERE iduser=%s', (escape_string(str(userid))))
 	conn.commit()
 	c.close()
 	conn.close()
 
 def del_organization(organizationid):
 	"""Function takes organization's id and deletes it from database
-	
+
 		tip: check dependencies before deleting
 	"""
 	c, conn = connection()
-	c.execute('DELETE from organization WHERE idorganization=%s;', (escape_string(str(organizationid))))
+	c.execute('DELETE FROM organization WHERE idorganization=%s', (escape_string(str(organizationid))))
 	conn.commit()
 	c.close()
 	conn.close()
@@ -110,7 +110,7 @@ def del_club(clubid):
 		tip: check dependencies before deleting
 	"""
 	c, conn = connection()
-	c.execute('DELETE from club WHERE idclub=%s;', (escape_string(str(clubid))))
+	c.execute('DELETE FROM club WHERE idclub=%s', (escape_string(str(clubid))))
 	conn.commit()
 	c.close()
 	conn.close()
@@ -121,7 +121,23 @@ def del_event(eventid):
 		tip: check dependencies before deleting
 	"""
 	c, conn = connection()
-	c.execute('DELETE from event WHERE idevent=%s;', (escape_string(str(eventid))))
+	c.execute('DELETE FROM event WHERE idevent=%s', (escape_string(str(eventid))))
+	conn.commit()
+	c.close()
+	conn.close()
+
+def del_user_from_club(userid, clubid):
+	"""Functions takes user's id and club's id and removes user from that club"""
+	c, conn = connection()
+	c.execute('DELETE FROM club_membership WHERE user_id=%s and club_id=%s', (escape_string(str(userid)), escape_string(str(userid))))
+	conn.commit()
+	c.close()
+	conn.close()
+
+def del_user_from_event(userid, eventid):
+	"""Functions takes user's id and event's id and removes user from that event"""
+	c, conn = connection()
+	c.execute('DELETE FROM event_membership WHERE user_id=%s and event_id=%s', (escape_string(str(userid)), escape_string(str(userid))))
 	conn.commit()
 	c.close()
 	conn.close()
