f13c602238957b3756da4b2f730e76a22bf48e9a
tykazzz <tlakomy2001@gmail.com>
2018-11-06 13:35:01 +0100

    change in database functions

--- pyclub/dbconnect.py
+++ pyclub/dbconnect.py
@@ -321,6 +321,28 @@ def give_admin(userid):
 def change_mail(userid, new_mail):
 	c, conn = connection()
 	c.execute('UPDATE user SET email=%s WHERE iduser=%s', (escape_string(new_mail), escape_string(str(userid))))
+	c.execute('UPDATE user SET email_confirm=0 WHERE iduser=%s', escape_string(str(userid)))	
+	conn.commit()
+	c.close()
+	conn.close()
+
+def change_event_info(eventid, new_info):
+	c, conn = connection()
+	c.execute('UPDATE event SET info=%s WHERE idevent=%s', (escape_string(new_info), escape_string(str(str(eventid)))))
+	conn.commit()
+	c.close()
+	conn.close()
+
+def change_organization_contact(organizationid, new_contact):
+	c, conn = connection()
+	c.execute('UPDATE organization SET contact=%s WHERE idorganization=%s', (escape_string(new_contact), escape_string(str(str(organizationid)))))
+	conn.commit()
+	c.close()
+	conn.close()
+
+def change_user_password(userid, new_password):
+	c, conn = connection()
+	c.execute('UPDATE user SET password=%s WHERE iduser=%s', (escape_string(new_password), escape_string(str(str(userid)))))
 	conn.commit()
 	c.close()
 	conn.close()
