92fe122a135857a000f09e9c217a2162cca67bcf
tykazzz <tlakomy2001@gmail.com>
2018-11-15 12:29:40 +0100

    add functions

--- pyclub/dbconnect/change.py
+++ pyclub/dbconnect/change.py
@@ -12,6 +12,7 @@ def confirm_email(mail):
 	conn.close()
 
 def give_admin(userid):
+	'''Function takes userid and gives him all priviliges'''
 	c, conn = connection()
 	c.execute('UPDATE user SET admin=1 WHERE iduser=%s', (escape_string(str(userid))))
 	conn.commit()
@@ -19,6 +20,7 @@ def give_admin(userid):
 	conn.close()
 
 def change_mail(userid, new_mail):
+	'''Function takes userid and new mail and changes specified user mail'''
 	c, conn = connection()
 	c.execute('UPDATE user SET email=%s WHERE iduser=%s', (escape_string(new_mail), escape_string(str(userid))))
 	c.execute('UPDATE user SET email_confirm=0 WHERE iduser=%s', escape_string(str(userid)))
@@ -27,6 +29,7 @@ def change_mail(userid, new_mail):
 	conn.close()
 
 def change_event_info(eventid, new_info):
+	'''Function takes eventid and new info and changes specified event info'''
 	c, conn = connection()
 	c.execute('UPDATE event SET info=%s WHERE idevent=%s', (escape_string(new_info), escape_string(str(str(eventid)))))
 	conn.commit()
@@ -34,6 +37,7 @@ def change_event_info(eventid, new_info):
 	conn.close()
 
 def change_organization_contact(organizationid, new_contact):
+	'''Function takes organizationid and new contact and changes specified organization contact'''
 	c, conn = connection()
 	c.execute('UPDATE organization SET contact=%s WHERE idorganization=%s', (escape_string(new_contact), escape_string(str(str(organizationid)))))
 	conn.commit()
@@ -41,6 +45,7 @@ def change_organization_contact(organizationid, new_contact):
 	conn.close()
 
 def change_user_password(userid, new_password):
+	'''Function takes userid and new password and changes specified user password'''
 	c, conn = connection()
 	c.execute('UPDATE user SET password=%s WHERE iduser=%s', (escape_string(new_password), escape_string(str(str(userid)))))
 	conn.commit()
@@ -48,6 +53,7 @@ def change_user_password(userid, new_password):
 	conn.close()
 
 def change_event_date(eventid, new_date):
+	'''Function takes eventid and new date and changes specified event date'''
 	c, conn = connection()
 	c.execute('UPDATE event SET date=%s WHERE idevent=%s', (escape_string(new_date), escape_string(str(str(eventid)))))
 	conn.commit()
@@ -55,6 +61,7 @@ def change_event_date(eventid, new_date):
 	conn.close()
 
 def give_club_ownership(userid, clubid):
+	'''Function takes userid and clubid and set user as owner of that club'''
 	c, conn =  connection()
 	c.execute('UPDATE club SET owner_id=%s WHERE idclub=%s', (escape_string(str(userid)), escape_string(str(clubid))))
 	conn.commit()
@@ -62,6 +69,7 @@ def give_club_ownership(userid, clubid):
 	conn.close()
 
 def give_event_ownership(userid, eventid):
+	'''Function takes userid and eventid and set user as owner of that event'''
 	c, conn =  connection()
 	c.execute('UPDATE event SET owner_id=%s WHERE idevent=%s', (escape_string(str(userid)), escape_string(str(eventid))))
 	conn.commit()
@@ -69,6 +77,7 @@ def give_event_ownership(userid, eventid):
 	conn.close()
 
 def give_organization_ownership(userid, organizationid):
+	'''Function takes userid and organizationid and set user as owner of that organization'''
 	c, conn =  connection()
 	c.execute('UPDATE organization SET owner_id=%s WHERE idorganization=%s', (escape_string(str(userid)), escape_string(str(organizationid))))
 	conn.commit()
