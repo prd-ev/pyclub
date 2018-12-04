b31c49020436b57de1b0e3cc7e3696751164fa74
Konstanty Kordas <kostekkor@gmail.com>
2018-12-04 12:22:30 +0100

    cos na pewno

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, url_for, redirect, session, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from pyclub.dbconnect import create_user, confirm_email, get_user, create_organization, get_organization_by_name, create_club, create_club_membership, get_club, get_club_by_organization, create_event, get_club_membership, give_club_ownership, create_event_membership, give_event_ownership, get_event, get_all_organizations
+from pyclub.dbconnect import create_user, confirm_email, get_user, create_organization, get_organization_by_name, create_club, create_club_membership, get_club, get_club_by_organization, create_event, get_club_membership, give_club_ownership, create_event_membership, give_event_ownership, get_event, get_all_organizations, get_club_membership, get_events_by_club, get_event_membership, give_admin
 from werkzeug.security import generate_password_hash, check_password_hash
 from email_confirmation import confirm_token, send_email_authentication
 from main import app
@@ -96,7 +96,6 @@ def organization_page(organization_name):
     current_organization_name = organization_dict.get('name')
     current_organization_id = organization_dict.get('idorganization')
     current_organization_club_list = get_club_by_organization(current_organization_id)
-    print(current_organization_club_list)
     return render_template('organization_profile.html', name = current_organization_name, contact = current_organization_contact, list = current_organization_club_list)
 
 @app.route('/organizations/<organization_name>/new_club/', methods = ["POST", "GET"])
@@ -123,7 +122,15 @@ def add_club_page(organization_name):
 def club_page(organization_name, club_name):
     current_club_dict = get_club(club_name)
     current_club_info = current_club_dict.get('info')
-    return render_template('club_profile.html', club_name=club_name, info=current_club_info,organization_name=organization_name)
+    current_club_id = current_club_dict.get('idclub')
+    members_list_id = get_club_membership(current_club_id)
+    members_list_emails = []
+    for user in members_list_id:
+        user_dict = get_user(user) 
+        user_email = user_dict.get('email')
+        members_list_emails.append(user_email)
+    current_club_evnts_list = get_events_by_club(current_club_id)
+    return render_template('club_profile.html', club_name=club_name, info=current_club_info,organization_name=organization_name, users_list=members_list_emails, events_list=current_club_evnts_list)
 
 @app.route('/organizations/<organization_name>/<club_name>/join/')
 def club_join_page(organization_name, club_name):
@@ -142,18 +149,15 @@ def new_event_page(organization_name, club_name):
     if request.method == 'POST':
         current_club_dict = get_club(club_name)
         current_club_id = current_club_dict.get('idclub')
-        print(current_club_id)
         new_event_name = request.form['event_name']
         new_event_date = request.form['event_date']
         new_event_info = request.form['event_info']
         new_event_date = new_event_date.split('T')
         new_event_date = '{} {}:00'.format(new_event_date[0],new_event_date[1])
-        print(new_event_date)
         if new_event_date and new_event_info and current_club_id and new_event_name:
             new_event_create = create_event(new_event_date, new_event_info, current_club_id,new_event_name)
             if new_event_create is None:
                 new_event_dict = get_event(new_event_name)
-                print(new_event_name)
                 new_event_id = new_event_dict.get('idevent')
                 create_event_membership(current_user.id,new_event_id)
                 give_event_ownership(current_user.id,new_event_id)
@@ -162,6 +166,45 @@ def new_event_page(organization_name, club_name):
     return render_template('add_event.html', organization_name=organization_name, club_name=club_name)
 
 
+@app.route('/organizations/<organization_name>/<club_name>/<event_name>')
+def event_page(organization_name, club_name, event_name):
+    event_dict = get_event(event_name)
+    event_date = event_dict.get('date')
+    event_info = event_dict.get('info')
+    event_id = event_dict.get('idevent')
+    members_list_id = get_event_membership(event_id)
+    members_list_emails = []
+    for user in members_list_id:
+        user_dict = get_user(user) 
+        user_email = user_dict.get('email')
+        members_list_emails.append(user_email)
+    
+    return render_template("event_profile.html", info=event_info, date=event_date, organization_name=organization_name, club_name=club_name, event_name=event_name, users_list=members_list_emails)
+
+@app.route('/organizations/<organization_name>/<club_name>/<event_name>/join/')
+def event_join_page(organization_name, club_name, event_name):
+    current_event_dict = get_event(event_name)
+    current_event_id = current_event_dict.get('idevent')
+    event_members = get_event_membership(current_event_id)
+    parent_club_dict = get_club(club_name)
+    parent_club_id = parent_club_dict.get('idclub')
+    parent_club_members = get_club_membership(parent_club_id)
+    if current_user.id not in parent_club_members:
+        flash('dołącz do klubu aby mieć dostęp do wydarzenia')
+        return redirect(url_for('club_page', organization_name=organization_name, club_name=club_name))
+    if current_user.id in event_members:
+        flash('Jesteś już członkiem wydarzenia')
+    else:
+        create_event_membership(current_user.id,current_event_id)
+    return redirect(url_for('event_page', organization_name=organization_name, club_name=club_name, event_name=event_name))
+
+
+
+@app.route('/test/')
+def test():
+    give_admin(current_user.id)
+    x = current_user.get_id
+    return str(x)
 
 @app.route('/logout/')
 def logout():
